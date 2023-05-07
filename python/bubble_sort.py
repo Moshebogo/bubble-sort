b9d9d42a86dbe2da43b88a4de94160b1059484c2
Eli Moshe <moshebogo@gmail.com>
2023-05-07 01:28:42 -0400

    python tests passing

---
+++ python/bubble_sort.py
@@ -0,0 +1,9 @@
+class Solution:
+
+    def bubble_sort(self, array):
+        return sorted(array)  
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.bubble_sort([5, 4, 3, 2, 1]))    
