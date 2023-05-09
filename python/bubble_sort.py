597edd40b2a214821837b1a2dfa0699181dc403c
Eli Moshe <moshebogo@gmail.com>
2023-05-08 22:49:22 -0400

    sort of working

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -1,9 +1,32 @@
 class Solution:
 
-    def bubble_sort(self, array):
+    def bubble_sort1(self, array):
         return sorted(array)  
+    
+    def bubble_sort2(self, array):
 
+        for i in range(len(array)):
+            current = array[i]
+            try:
+                next = array[i+1]
+            except IndexError:
+                next = None
+            try:
+                if current > next:
+                    temp1 = array[i:i+1]
+                    temp2 = array[i+1:i+2]
+                    x = array.pop(i+1)
+                    print(x)
+                    array.insert(i, temp2[0])
+                 
+            except TypeError:
+                pass  
+             
+          
+
+        return array
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.bubble_sort([5, 4, 3, 2, 1]))    
+    # print("expecting =>", [1, 2, 3, 4, 5])
+    print(s.bubble_sort2([5, 4, 3, 2, 1]))    
